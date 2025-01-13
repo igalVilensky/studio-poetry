79a7cfa2c7ee0335b32702bd0c857d9574d48e0d
Igal <igal.vilensky@flowplace.com>
2025-01-13 23:53:42 +0100

    update and correct schema

--- schemaTypes/track.js
+++ schemaTypes/track.js
@@ -1,22 +1,24 @@
 // schemaTypes/track.js
-export default {
+import {defineField, defineType} from 'sanity'
+
+export const trackType = defineType({
   name: 'track',
   type: 'document',
   title: 'Track',
   fields: [
-    {
+    defineField({
       name: 'title',
       type: 'string',
       title: 'Title',
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'description',
       type: 'text',
       title: 'Description',
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'audioFile',
       type: 'file',
       title: 'Audio File',
@@ -24,14 +26,14 @@ export default {
         accept: 'audio/mpeg', // Only allow MP3 files
       },
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'coverImage',
       type: 'image',
       title: 'Cover Image',
       options: {
         hotspot: true, // Enable image cropping
       },
-    },
+    }),
   ],
-}
+})
